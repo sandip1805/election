cabeebcf275631622117992dba23d09d21bae1d8
Sandip <sandip1805@gmail.com>
2022-06-21 22:34:12 +0530

    first commit

---
+++ contracts/Election.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.0;
+
+contract Election {
+
+    string public candidate;
+
+    constructor() {
+        candidate = "Candidate 1";
+    }
+
+}
